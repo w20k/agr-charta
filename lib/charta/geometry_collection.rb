ab3a3f807763bd43639adda944305105a291bba4
nicolas PROCUREUR <nprocureur@gmail.com>
2017-09-29 11:30:11 +0200

    Adds a way to give features properties while converting into geojson feature collection

--- lib/charta/geometry_collection.rb
+++ lib/charta/geometry_collection.rb
@@ -7,11 +7,14 @@ module Charta
       new(feature)
     end
 
-    def to_json_feature_collection
-      features = []
-      feature.each do |f|
-        features << Charta.new_geometry(f).to_json_feature
+    def to_json_feature_collection(collection_properties = [])
+      features = feature.each.with_index.collect do |f,i|
+        properties = {}
+        properties = collection_properties[i] unless collection_properties[i].nil?
+
+        Charta.new_geometry(f).to_json_feature(properties)
       end
+
       { type: 'FeatureCollection', features: features }
     end
   end
